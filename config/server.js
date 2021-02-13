4cd90073899f9021f8a0f13701261b45c339a3fe
richpjames <rich@Richards-MacBook-Pro.local>
2021-02-13 20:14:11 +0000

    change dev db

--- config/server.js
+++ config/server.js
@@ -1,9 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '733c1a756ca5beed2c20c52f94da6035'),
-    },
-  },
+  url: env("HEROKU_URL"),
 });
