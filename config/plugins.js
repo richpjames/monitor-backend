3d8aff543518b25f8d4243ba115fe604620720cf
richpjames <rich@Richards-MacBook-Pro.local>
2021-03-17 21:07:17 +0000

    add cloudinary stuff

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => {
+  return {
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env["CLOUDINARY_NAME"],
+        api_key: process.env["CLOUDINARY_KEY"],
+        api_secret: process.env["CLOUDINARY_SECRET"],
+      },
+    },
+  };
+  // ...
+};
